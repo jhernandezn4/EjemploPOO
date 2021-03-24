88f58576a2b116065eecd1f7e849ca6a58349cc1
Jorge Asus <jorgehn1995@gmail.com>
2021-03-24 10:16:16 -0600

    Agregar archivos de proyecto.

---
+++ EjemploPOO/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EjemploPOO.clases;
+
+
+namespace EjemploPOO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void inicio_Click(object sender, EventArgs e)
+        {
+            ClsAutomovil carrito = new ClsAutomovil();
+            carrito.marca = "Meches";
+            carrito.color = "Blanco";
+            carrito.vel_max = 100;
+            MessageBox.Show("Carrito creado con el color " + carrito.color);
+        }
+    }
+}
---
+++ EjemploPOO/clases/ClsAutomovil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPOO.clases
+{
+    class ClsAutomovil
+    {
+        public String marca { get; set; }
+        public int vel_max { get; set; }
+
+        public int velocidadActual { get; set; }
+        public bool encendido { get; set; }
+
+        public string color { get; set; }
+
+
+
+        public void EncenderMotor()
+        {
+            encendido = true;
+            vel_max = 0;
+        }
+    }
+}
